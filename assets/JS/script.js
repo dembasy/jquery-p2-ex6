c658c7c41837bf17cfd8b7f3772ed3bb02dfa33c
dembasy <dembasy1227@gmail.com>
2018-06-28 15:11:16 +0200

    fin de l'exercice6

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+$('#green').mouseover(function(){
+  $('#text').css('color', 'green');
+  });
+$('#red').mouseover(function(){
+  $('#text').css('color', 'red');
+  });
+$('#blue').mouseover(function(){
+  $('#text').css('color', 'blue');
+  });
